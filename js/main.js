7d2a5b77dffc8ee482f4980fd0429231f84bcf28
Jake Wayne <jhwayne@umich.edu>
2014-10-06 16:43:31 -0400

    one div showing at a time

--- js/main.js
+++ js/main.js
@@ -169,23 +169,22 @@ $( document ).ready(function() {
  //    }
 
  //  }).call(this);
+
 $('a[href^="#"]').click(function(event) {
 	var id = $(this).attr("href");
 	var target = $(id).offset().top;
-	$('html, body').animate({scrollTop:target}, 500);
-	event.preventDefault();
+	if($(id).hasClass("hidden")){
+		$(".active").addClass("hidden");
+		$(id).removeClass("hidden");
+		$(id).addClass("active");
+	}
+
+	
+	// $('html, body').animate({scrollTop:target}, 500);
+	// event.preventDefault();
 });
 
-var offset = $('nav').offset().top;
-$(window).scroll(function() {
-	if($(this).scrollTop() >= offset) {
-		$('nav').addClass('isFixed');
-		$('html').addClass('whiteSpace');
-	} else {
-		$('nav').removeClass('isFixed');
-		$('html').removeClass('whiteSpace');
-	}
-});
+
 });
 
 
---
+++ test.js
@@ -0,0 +1,23 @@
+jQuery(function($) {
+
+    $('a.panel').click(function() {
+        var $target = $($(this).attr('href')),
+            $other = $target.siblings('.active');
+        
+        if (!$target.hasClass('active')) {
+            $other.each(function(index, self) {
+                var $this = $(this);
+                $this.removeClass('active').animate({
+                    left: $this.width()
+                }, 500);
+            });
+
+            $target.addClass('active').show().css({
+                left: -($target.width())
+            }).animate({
+                left: 0
+            }, 500);
+        }
+    });
+
+});
