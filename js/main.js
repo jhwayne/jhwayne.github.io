61cc107d55c911c502a3a1f2a4ca553717d830ac
Jake Wayne <jhwayne@umich.edu>
2014-10-07 00:10:54 -0400

    reformatted container divs

--- js/main.js
+++ js/main.js
@@ -9,46 +9,56 @@ $('a[href^="#"]').click(function(event) {
 		$(id).addClass("active");
 	}
 	if ($(this).attr("href") == "#about"){
-		$("nav").css("background","#3498db");
-		$("nav li a").css("background","#3498db");
-		$("body").css("background","#3498db");
+		// $("nav").css("background","#3498db");
+		// $("nav li a").css("background","#3498db");
+		// $("body").css("background","#3498db");
 		$("section").css("background","#3498db");
-		$("header").css("background","#3498db");
-		$("footer").css("background","#3498db");
-		$("html").css("background","#3498db");
-		$(".icon-button").css("background","#3498db")
+		// $("header").css("background","#3498db");
+		// $("footer").css("background","#3498db");
+		// $("html").css("background","#3498db");
+		// $(".icon-button").css("background","#3498db")
+	}
+	else if ($(this).attr("href") == "#home"){
+		// $("nav").css("background","#ffd071");
+		// $("nav li a").css("background","#ffd071");
+		// $("body").css("background","#ffd071");
+		$("section").css("background","#ffffff");
+		// $("header").css("background","#ffd071");
+		// $("footer").css("background","#ffd071");
+		// $("html").css("background","#ffd071");
+		// $(".icon-button").css("background","#ffd071")
 	}
 	else if ($(this).attr("href") == "#work"){
-		$("nav").css("background","#ffd071");
-		$("nav li a").css("background","#ffd071");
-		$("body").css("background","#ffd071");
+		// $("nav").css("background","#ffd071");
+		// $("nav li a").css("background","#ffd071");
+		// $("body").css("background","#ffd071");
 		$("section").css("background","#ffd071");
-		$("header").css("background","#ffd071");
-		$("footer").css("background","#ffd071");
-		$("html").css("background","#ffd071");
-		$(".icon-button").css("background","#ffd071")
+		// $("header").css("background","#ffd071");
+		// $("footer").css("background","#ffd071");
+		// $("html").css("background","#ffd071");
+		// $(".icon-button").css("background","#ffd071")
 	}
 
 	else if ($(this).attr("href") == "#experience"){
-		$("nav").css("background","#f0776c");
-		$("nav li a").css("background","#f0776c");
-		$("body").css("background","#f0776c");
+		// $("nav").css("background","#f0776c");
+		// $("nav li a").css("background","#f0776c");
+		// $("body").css("background","#f0776c");
 		$("section").css("background","#f0776c");
-		$("header").css("background","#f0776c");
-		$("footer").css("background","#f0776c");
-		$("html").css("background","#f0776c");
-		$(".icon-button").css("background","#f0776c")
+		// $("header").css("background","#f0776c");
+		// $("footer").css("background","#f0776c");
+		// $("html").css("background","#f0776c");
+		// $(".icon-button").css("background","#f0776c")
 	}
 
 	else if ($(this).attr("href") == "#contact"){
-		$("nav").css("background","#1abc9c");
-		$("nav li a").css("background","#1abc9c");
-		$("body").css("background","#1abc9c");
+		// $("nav").css("background","#1abc9c");
+		// $("nav li a").css("background","#1abc9c");
+		// $("body").css("background","#1abc9c");
 		$("section").css("background","#1abc9c");
-		$("header").css("background","#1abc9c");
-		$("footer").css("background","#1abc9c");
-		$("html").css("background","#1abc9c");
-		$(".icon-button").css("background","#1abc9c")
+		// $("header").css("background","#1abc9c");
+		// $("footer").css("background","#1abc9c");
+		// $("html").css("background","#1abc9c");
+		// $(".icon-button").css("background","#1abc9c")
 	}
 
 
